21fba77f58f785ebee5e42901800cb2c4ac81958
ppxan <37857700+ppxan@users.noreply.github.com>
2021-03-28 22:55:37 +0800

    fix type and add backend (#2)
    * add mongo
    
    * add mongo
    
    * add mongo
    
    * add mongo
    
    * add mongo
    
    * add mongo
    
    * add mongo
    
    * add mongo
    
    * add mongo
    
    * add mongo
    
    * add mongo
    
    * add mongo
    
    * add mongo
    
    * add mongo
    
    * add mongo
    
    * add mongo
    
    * add mongo
    
    * add mongo
    
    * add mongo
    
    * add mongo
    
    * add mongo
    
    * add mongo
    
    * add mongo
    
    * fix type
    
    Co-authored-by: xudashuai <xudashuai0819@outlook.com>

--- server/index.js
+++ server/index.js
@@ -1,11 +1,51 @@
-const express = require('express')
-const app = express()
-const port = 80
+const express = require('express');
+var bodyParser = require('body-parser');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const app = express();
+app.use(bodyParser.json());
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+const port = 80;
+const { MongoClient, ObjectId } = require('mongodb');
+
+const url = 'mongodb://root:root@mongo:27017';
+
+(async function () {
+  const client = await MongoClient.connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  const db = client.db('startup');
+
+  // load form
+  app.get('/form/:id', async (req, res) => {
+    console.log('get form' + JSON.stringify(req.params));
+    const result = await db
+      .collection('form')
+      .find({ _id: new ObjectId(req.params.id) })
+      .toArray();
+    console.log('get form' + JSON.stringify(req.params));
+    res.send(result);
+  });
+
+  // create form
+  app.post('/form', async (req, res) => {
+    console.log('post form' + req.body);
+    const result = await db.collection('form').insertOne(req.body);
+    res.json(result);
+  });
+
+  // submit form
+  app.post('/submit/:id', async (req, res) => {
+    const result = await db.collection('form').updateOne(
+      { _id: ObjectId(req.params.id) },
+      {
+        $push: {
+          result: req.body,
+        },
+      }
+    );
+    res.json(result);
+  });
+
+  app.listen(port, () => {});
+})();
