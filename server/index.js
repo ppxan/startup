9f6bd856d267acb23390b5711c21a48dac04ce6e
xudashuai <xudashuai0819@outlook.com>
2021-03-28 17:51:26 +0800

    add mongo

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 80;
 const { MongoClient } = require('mongodb');
 const url = 'mongodb://root:root@mongo:27017';
 
