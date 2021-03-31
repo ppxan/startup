94a6d02610ff1eec89f59920745d8e9ffec68920
xudashuai <xudashuai0819@outlook.com>
2021-03-31 19:47:54 +0800

    add submit api

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -8,3 +8,15 @@ const axios = Axios.create({
 export function getForm(id: string, config?: AxiosRequestConfig) {
   return axios.get<Form[]>(`/form/${id}`, config).then((res) => res.data[0]);
 }
+
+export function submitForm(id: string, data: any, config?: AxiosRequestConfig) {
+  return axios
+    .post<{ result: { ok: number } }>(`/submit/${id}`, data, config)
+    .then((res) => res.data.result);
+}
+
+export function createForm(id: string, form: any, config?: AxiosRequestConfig) {
+  return axios
+    .post<{ result: { ok: number } }>(`/form/${id}`, form, config)
+    .then((res) => res.data.result);
+}
